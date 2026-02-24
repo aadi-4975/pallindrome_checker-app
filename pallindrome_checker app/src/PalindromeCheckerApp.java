1daed67b18d5f480d95491a286caa7095589005e
aadi-4975 <toofani4975@gmail.com>
2026-02-24 11:23:13 +0530

    UC8:Hardcoded palindrome

--- pallindrome_checker app/src/PalindromeCheckerApp.java
+++ pallindrome_checker app/src/PalindromeCheckerApp.java
@@ -1,24 +1,17 @@
-import java.util.Stack;
-import java.util.Queue;
 import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        String input = "civic";
-
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
-
+        String input = "level";
+        LinkedList<Character> list = new LinkedList<>();
         for (char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
+            list.add(c);
         }
 
         boolean isPalindrome = true;
-
-        while (!queue.isEmpty()) {
-            if (!queue.remove().equals(stack.pop())) {
+        while (list.size() > 1) {
+            if (!list.removeFirst().equals(list.removeLast())) {
                 isPalindrome = false;
                 break;
             }
