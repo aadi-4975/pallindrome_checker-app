ba3dc152518324d44e7f33dcd956d5cb388cf6d5
aadi-4975 <toofani4975@gmail.com>
2026-02-24 11:13:41 +0530

    UC6:Hardcoded palindrome

--- pallindrome_checker app/src/PalindromeCheckerApp.java
+++ pallindrome_checker app/src/PalindromeCheckerApp.java
@@ -1,20 +1,29 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        String input = "noon";
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
+
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
+
         boolean isPalindrome = true;
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
         }
+
         System.out.println("Input: " + input);
         System.out.println("Is it a palindrome? " + isPalindrome);
     }
