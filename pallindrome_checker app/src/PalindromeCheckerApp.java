f02dfb6c5781a11bf8ba5175b0b56fa421303ef4
aadi-4975 <toofani4975@gmail.com>
2026-03-11 10:27:56 +0530

    UC13: Add welcome message module

--- pallindrome_checker app/src/PalindromeCheckerApp.java
+++ pallindrome_checker app/src/PalindromeCheckerApp.java
@@ -8,11 +8,9 @@ class StackStrategy implements PalindromeStrategy {
     @Override
     public boolean check(String input) {
         java.util.Stack<Character> stack = new java.util.Stack<>();
-
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
-
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
                 return false;
@@ -27,7 +25,18 @@ public class PalindromeCheckerApp {
         String input = "level";
         PalindromeStrategy strategy = new StackStrategy();
 
+        long startTime = System.nanoTime();
+
+        boolean result = strategy.check(input);
+
+
+        long endTime = System.nanoTime();
+
+
+        long duration = endTime - startTime;
+
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + strategy.check(input));
+        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + duration + " ns");
     }
 }
